c4c1d5f14d3cc133fdc7fcb23f25f69fef67a24d
Abhiraj Shourya <45894478+abhirajshourya@users.noreply.github.com>
2021-05-21 02:26:10 +0530

    Update 2.4

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+
+    //cached reference
+    Level level;
+    
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision) 
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+        level.BlockDestoryed();
     }
 }
---
+++ Block Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    //Counters-Trackers
+    [SerializeField] int breakableBlocks; //Serialized for debugging purposes
+    
+    //cached reference
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void BlockDestoryed()
+    {
+        breakableBlocks--;
+        if(breakableBlocks<=0)
+        {
+            sceneLoader.LoadNextScreen();
+        }
+    }
+}
