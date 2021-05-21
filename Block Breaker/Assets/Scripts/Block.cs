51a94bf03063001d336ff9400e2eb7ef050cd1bf
Abhiraj Shourya <45894478+abhirajshourya@users.noreply.github.com>
2021-05-22 03:04:38 +0530

    Update 3
    Game is fairly playable now. Needs more polishing.
    
    Co-Authored-By: rahulkrbxr <61363513+rahulkrbxr@users.noreply.github.com>

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -4,28 +4,77 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    //config params
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparksVFX;
+    [SerializeField] int maxHits = 2; //Serialized for Debug
+    [SerializeField] Sprite[] hitSprites;
 
     //cached reference
     Level level;
-    
 
+    //state variables
+    [SerializeField] int timesHit; //Serialized for Debug
+    
     private void Start()
     {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
         level = FindObjectOfType<Level>();
-        level.CountBreakableBlocks();
+        if(tag == "breakable")
+        {
+            level.CountBlocks();
+        }
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision) 
     {
-        DestroyBlock();
+        if(tag == "breakable")
+        {
+            HandleHits();
+        }
+    }
+
+    private void HandleHits()
+    {
+        timesHit++;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit-1;
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
     }
 
     private void DestroyBlock()
     {
+        PlayBlockDestroySFX();
         FindObjectOfType<GameSession>().AddToScore();
-        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
         level.BlockDestoryed();
+        TriggerSparksVFX();
+    }
+
+    private void PlayBlockDestroySFX()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+
+    private void TriggerSparksVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparksVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
     }
 }
